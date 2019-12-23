599dcb3425ab94fba6a29cbeaff2333ea0c3ea5e
RubanovVasiliy <rubanovas2005@gmail.com>
2019-12-23 15:33:02 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int size) {
+	int swapped;
+	int temp;
+
+	do {
+		swapped = 0;
+		for (int i = 0; i < size - 1; i++) {
+			if (arr[i] > arr[i + 1]) {
+				temp = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = temp;
+				swapped = 1;
+			}
+		}
+	} while (swapped);
+}
+
+void printArray(int arr[], int size) {
+	for (int i = 0; i < size; i++) {
+		printf("%d ", arr[i]);
+	}
+}
+
+void inputArray(int arr[], int size) {
+	for (int i = 0; i < size; i++) {
+		scanf("%d", &arr[i]);
+	}
+}
+
+int main(void) {
+	printf("Input length of the array: ");
+	int size;
+	scanf("%d", &size);
+
+	printf("Input elements of array: ");
+	int arr[size];
+	inputArray(arr, size);
+
+	bubbleSort(arr, size);
+	printArray(arr, size);
+
+	return 0;
+}
